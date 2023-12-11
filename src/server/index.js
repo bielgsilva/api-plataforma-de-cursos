96c7b305671690043cc626946df0873faade2d1c
Gabriel Gomes Silva <ggsilva.eng@gmail.com>
2023-12-11 19:59:56 -0300

    npm

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 const routes = require("../routes/healthCheck");
 const userRoutes = require("../routes");
 
