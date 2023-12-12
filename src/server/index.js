087063590545d48e00675e12e6d2f8e01eaabf43
Gabriel Gomes Silva <ggsilva.eng@gmail.com>
2023-12-12 11:51:29 -0300

    start render

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,11 @@
 require("dotenv").config(); 
 const express = require("express");
-const app = express();
 const cors = require("cors");
 
 const health = require("../routes/health");
 const userRoutes = require("../routes");
+
+const app = express();
 
 app.use(express.json());
 app.use(cors());
