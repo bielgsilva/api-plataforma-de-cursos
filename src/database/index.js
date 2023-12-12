1369387efa97c267d237d81d7ffe713550c7a961
Gabriel Gomes Silva <ggsilva.eng@gmail.com>
2023-12-12 11:55:53 -0300

    FIX

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const knex = require('knex')({
     client: 'pg',
-    connection: "postgres://grbodgmn:ygn0--UiQCt8BRKnSqdKwc1CJe_Yuma5@tuffi.db.elephantsql.com/grbodgmn",
+    connection: process.env.DB_URI,
 })
 
 module.exports = knex
